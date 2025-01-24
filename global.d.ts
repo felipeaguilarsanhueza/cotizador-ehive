bc0833f74cc7614a894a703aadd7c3388ca1df05
felipeaguilarsanhueza <felipeaguilarsanhueza@gmail.com>
2025-01-24 16:22:29 -0300

    nuevo4

--- global.d.ts
+++ global.d.ts
@@ -1,2 +1,9 @@
 // global.d.ts
 declare module 'nodemailer';
+declare module "d3-fetch" {
+    export function csv(
+      url: string,
+      row?: (d: any, index: number, columns: string[]) => any
+    ): Promise<any[]>;
+  }
+  
